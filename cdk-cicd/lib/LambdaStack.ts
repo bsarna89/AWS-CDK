c93cb603b4bfb14cefb38bf4eab71ebb1fef5476
bsarna89 <bsarna89@gmail.com>
2024-06-06 16:41:29 +0100

    lambda-synth-fix-2

--- cdk-cicd/lib/LambdaStack.ts
+++ cdk-cicd/lib/LambdaStack.ts
@@ -14,14 +14,14 @@ export class LambdaStack extends Stack {
     constructor(scope: Construct, id: string, props: LambdaStackProps) {
         super(scope, id, props)
 
-        new NodejsFunction(this, 'hello-lambda', {
-            runtime: Runtime.NODEJS_18_X,
-            handler: 'handler',
-            entry: (join(__dirname, '..', 'services', 'hello.ts')),
-            environment: {
-                STAGE: props.stageName!
-            }
-        })
+        // new NodejsFunction(this, 'hello-lambda', {
+        //     runtime: Runtime.NODEJS_18_X,
+        //     handler: 'handler',
+        //     entry: (join(__dirname, '..', 'services', 'hello.ts')),
+        //     environment: {
+        //         STAGE: props.stageName!
+        //     }
+        // })
 
 
     }
